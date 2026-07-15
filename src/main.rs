5bd14de762798238425ece6116d4e76bde6db2ee
MrInformatic <maxundmoritz99199@gmail.com>
2026-07-16 01:35:58 +0200

    Change dependency

--- src/main.rs
+++ src/main.rs
@@ -2,76 +2,84 @@ use std::process::exit;
 use std::sync::{Arc, Mutex};
 use std::thread::{sleep, spawn};
 use std::time::Duration;
-use rdev::{listen, simulate, Button, EventType, ListenError};
+use mouce::common::{MouseButton, MouseEvent};
+use mouce::{Mouse, MouseActions};
 
 #[derive(Clone)]
 pub struct State {
     active: bool,
-    button: Option<Button>,
+    button: Option<MouseButton>,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl State {
     pub fn new() -> Self {
         Self {
             active: true,
-            button: None
+            button: None,
         }
     }
-    
-    pub fn stop_it(&mut self) -> bool {
+
+    fn stop_it(&mut self) -> bool {
         if self.button.is_some() {
             self.active = false;
             self.button = None;
             return true;
         }
-        
         false
     }
-    
-    pub fn set_button(&mut self, button: Button) {
+
+    fn set_button(&mut self, button: MouseButton) {
         if self.active {
             self.button = Some(button);
         }
     }
 }
 
-fn main() -> Result<(), ListenError> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let state = Arc::new(Mutex::new(State::new()));
-    
+
     {
         let state = state.clone();
-        
         spawn(move || {
+            let mouse = Mouse::new();
             loop {
                 let state = { state.lock().unwrap().clone() };
 
                 if !state.active {
-                    continue
+                    continue;
                 }
-                
+
                 if let Some(button) = state.button {
-                    let _ = simulate(&EventType::ButtonPress(button));
+                    let _ = mouse.press_button(button);
                     sleep(Duration::from_millis(5));
-                    let _ = simulate(&EventType::ButtonRelease(button));
+                    let _ = mouse.release_button(button);
                     sleep(Duration::from_millis(5));
                 }
             }
         });
     }
 
-    {
-        listen(move |event| {
-            let mut state = state.lock().unwrap();
-            
-            match event.event_type {
-                EventType::ButtonPress(button) => {
-                    state.set_button(button);
-                }
-                EventType::MouseMove { .. } if state.stop_it() => {
-                    exit(0)
-                }
-                _ => {}
+    let mut mouse = Mouse::new();
+    mouse.hook(Box::new(move |event| {
+        let mut state = state.lock().unwrap();
+        match event {
+            MouseEvent::Press(button) => {
+                state.set_button(*button);
             }
-        })
+            MouseEvent::AbsoluteMove(_, _) if state.stop_it() => {
+                exit(0);
+            }
+            _ => {}
+        }
+    }))?;
+
+    loop {
+        sleep(Duration::from_secs(1));
     }
 }
